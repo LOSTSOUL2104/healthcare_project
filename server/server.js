6fc93084997e4682c15fe662d0a768b2909d948f
LOSTSOUL2104 <godofwar210405@gmail.com>
2024-10-23 15:27:31 +0530

    mainwed_work

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const path = require("path");
-
+const hbs = require("hbs");
 const app = express();
 const port = process.env.PORT || 5000;
 connectDb();
@@ -15,6 +15,7 @@ app.use(cors());
 app.set("view engine", "hbs");
 
 // ROUTES
+hbs.registerPartials(path.join(__dirname, "/views/partials"));
 app.get("/", (req, res) => {
   res.send("working");
 });
@@ -22,10 +23,17 @@ app.get("/home",(req,res) =>{
   res.render("home",{})
 })
 app.get("/allusers", (req, res) => {
-  res.render("allusers", {});
+  res.render("users", {
+    users: [
+      { id: 1, username: "priyansh", age: 19 },
+      { id: 2, username: "priyansh2", age: 20 },
+    ],
+  });
 });
+
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
+
 // ERROR HANDLING MIDDLEWARE should be added after routes
 app.use(errorHandler);
 
